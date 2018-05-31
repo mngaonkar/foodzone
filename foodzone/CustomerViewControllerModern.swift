09b057b25ca504db1ad64610755a787fbb2dd524
mahadev gaonkar <mahadev@mahadevs-MacBook-Pro.local>
2018-05-31 23:53:11 +0530

    customer screen

---
+++ foodzone/CustomerViewControllerModern.swift
@@ -0,0 +1,42 @@
+//
+//  CustomerViewControllerModern.swift
+//  foodzone
+//
+//  Created by mahadev gaonkar on 31/05/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+class CustomerViewControllerModern: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func scanClicked(_ sender: Any) {
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
