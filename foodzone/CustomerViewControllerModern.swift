5f095eeab602e8fcac0593205733c14cf7fff115
mahadev gaonkar <mahadev@mahadevs-MacBook-Pro.local>
2018-06-03 15:39:38 +0530

    customer screen, scan added

--- foodzone/CustomerViewControllerModern.swift
+++ foodzone/CustomerViewControllerModern.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
+import SwiftyJSON
+import Alamofire
 
-class CustomerViewControllerModern: UIViewController {
+class CustomerViewControllerModern: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
+    var session : AVCaptureSession!
+    var video : AVCaptureVideoPreviewLayer!
+    @IBOutlet weak var chefDataStatus: UILabel!
+    var foodQRCode : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,6 +37,107 @@ class CustomerViewControllerModern: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func scanPressed(_ sender: UIButton) {
+        session = AVCaptureSession()
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(input)
+        }
+        catch {
+            print("Error in capture device")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video?.frame = view.layer.bounds
+        view.layer.addSublayer(video!)
+        session.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects != nil && metadataObjects.count != 0 {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
+                if object.type == AVMetadataObject.ObjectType.qr {
+                    let alert = UIAlertController(title: "Food ID", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in UIPasteboard.general.string = object.stringValue
+                        self.chefDataStatus.text = "Lobster ID = \(object.stringValue!)"
+                        self.foodQRCode = Int(object.stringValue!)!
+                        self.session?.stopRunning()
+                        self.video.removeFromSuperlayer()
+                        self.getChefData(url: ServiceEndpoint().endPoint, param: ["LobsterId":self.foodQRCode])
+                    }))
+                    present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    // Get chef entered data from service
+    func getChefData(url : String, param : [String:Any]) {
+        let chefEnteredInfo = ChefDataModel()
+        var endpoint = url
+        endpoint.append("/v1/GetChefInfo")
+        print(endpoint)
+        print(param)
+        
+        Alamofire.request(endpoint, method: .get, parameters: param).responseData {
+            response in
+            if response.result.isSuccess {
+                print("Chef data received")
+                let chefData : JSON = JSON(response.result.value!)
+                
+                if chefData["chef_name"].exists(){
+                    chefEnteredInfo.chefName = chefData["chef_name"].string!
+                }
+                
+                if chefData["chef_experience"].exists(){
+                    chefEnteredInfo.chefExperience = chefData["chef_experience"].int!
+                }
+                
+                if chefData["cooking_care"].exists(){
+                    for item in chefData["cooking_care"].array! {
+                        chefEnteredInfo.cookingCare.append(item.string!)
+                    }
+                }
+                
+                if chefData["herbs_added"].exists(){
+                    for item in chefData["herbs_added"].array! {
+                        chefEnteredInfo.herbsAdded.append(item.string!)
+                    }
+                }
+                
+                if chefData["cooking_oil"].exists(){
+                    chefEnteredInfo.cookingOil = chefData["cooking_oil"].string!
+                }
+                
+                if chefData["lobster_id"].exists(){
+                    chefEnteredInfo.foodID = chefData["lobster_id"].int!
+                }
+            }
+            else {
+                print("Network error = \(response.result.error)")
+                self.chefDataStatus.text = "Not able to get chef information"
+            }
+        }
+        
+        // Test data
+        /*
+         let chefData : JSON = JSON(["chef_name": "mahadev",
+         "chef_experience":16,
+         "cooking_oil": "Single",
+         "herbs_added": ["cinnamon"],
+         "lobster_id": "9004686480",
+         "cooking_care": ["Low Flame", "Low Spices"]])
+         updateChefData(chefData: chefEnteredInfo)
+         */
+    }
     /*
     // MARK: - Navigation
 
